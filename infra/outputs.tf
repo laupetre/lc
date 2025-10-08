c55bf65e57af3291f6e37c6b1aedaf924c13584e
x13 <you@example.com>
2025-10-07 21:38:57 -0400

    1

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,20 +1,17 @@
-output "api_url" {
-  description = "Public URL of the Container App"
-  value       = "https://${azurerm_container_app.api.ingress[0].fqdn}"
+output "api_hostname" {
+  description = "Container App public host name"
+  value       = azurerm_container_app.api.latest_revision_fqdn
 }
 
-output "swa_url" {
-  description = "Static Web App default URL"
-  value       = "https://${azurerm_static_web_app.swa.default_host_name}"
+output "api_url" {
+  description = "Container App public URL"
+  value       = "https://${azurerm_container_app.api.latest_revision_fqdn}"
+}
+
+output "container_app_id" {
+  value = azurerm_container_app.api.id
 }
 
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
-}
-
-# Deployment token used by Azure/static-web-apps-deploy@v1
-output "swa_deployment_token" {
-  description = "SWA deployment token (mask & store as needed)"
-  value       = jsondecode(azapi_resource_action.swa_secrets.output).properties.apiKey
-  sensitive   = true
 }
