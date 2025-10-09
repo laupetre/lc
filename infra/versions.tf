80ef5aa487bcfbd8fcdbb91e761da2e03d88779a
x13 <you@example.com>
2025-10-08 20:26:54 -0400

    1

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,21 +1,14 @@
 terraform {
-  required_version = ">= 1.4.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.113"
+      version = "~> 3.111"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "~> 1.13"
+      version = "~> 1.12"
     }
   }
-
-  # Backend details are passed from the workflow via -backend-config
-  backend "azurerm" {}
-}
-
-provider "azurerm" {
-  features {}
 }
