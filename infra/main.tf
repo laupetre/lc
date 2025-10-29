de4f0d0afde80f367d8b68d7c820557ba4d5419a
x13 <you@example.com>
2025-10-29 07:21:27 -0400

    1

--- infra/main.tf
+++ infra/main.tf
@@ -21,7 +21,8 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
-  admin_enabled       = true  # Enable admin access for simpler authentication
+  # Enable admin access for simpler authentication
+  admin_enabled       = true
   tags                = local.tags
 }
 
